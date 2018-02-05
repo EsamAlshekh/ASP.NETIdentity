71871dea9052fafdf744dc04cd35785cd8a01548
Esam.Alshekh.1984 <Esam.Alshekh.1984@gmail.com>
2018-02-05 07:18:24 -0800

    Add project files.

---
+++ ASP.NETIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NETIdentity.Startup))]
+namespace ASP.NETIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
